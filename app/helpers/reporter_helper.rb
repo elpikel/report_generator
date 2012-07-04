72c55f3229bf145d6b8510e1fc53128a1c3983b1
Arkadiusz Plichta <el.pikel@gmail.com>
2012-07-04 21:57:20 +0200

    project combobox helper

--- app/helpers/reporter_helper.rb
+++ app/helpers/reporter_helper.rb
@@ -1,2 +1,18 @@
 module ReporterHelper
+  def render_project_box selected_project
+    return unless User.current.logged?
+    projects = User.current.memberships.collect(&:project).compact.uniq
+    if projects.any?
+      s = '<select name="project">'
+      projects.each { |p|
+        if p.id.to_s == selected_project.to_s
+          s << "<option value=\""+p.id.to_s+"\" selected>"+p.name+"</option>"
+        else
+          s << "<option value=\""+p.id.to_s+"\">"+p.name+"</option>"
+        end
+      }
+      s << '</select>'
+      s.html_safe
+    end
+  end
 end
