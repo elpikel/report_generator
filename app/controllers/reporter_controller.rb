7c8de030fcbf28bc127dc636aa04b2bc53cfdf0c
Arkadiusz Plichta <el.pikel@gmail.com>
2012-07-04 21:55:05 +0200

    query fix

--- app/controllers/reporter_controller.rb
+++ app/controllers/reporter_controller.rb
@@ -5,8 +5,15 @@ class ReporterController < ApplicationController
   def index
     @filters = Filters.new(:from => params[:from], :to => params[:to], :project => params[:project])
     @user = User.current
+    if @filters.project.nil?
+      conditions = ["#{TimeEntry.table_name}.user_id = ? AND #{TimeEntry.table_name}.spent_on BETWEEN ? AND ?",
+                    @user.id, @filters.from, @filters.to]
+    else
+      conditions = ["#{TimeEntry.table_name}.user_id = ? AND #{TimeEntry.table_name}.project_id = ? AND #{TimeEntry.table_name}.spent_on BETWEEN ? AND ?",
+                    @user.id, @filters.project, @filters.from, @filters.to]
+    end
     @entries = TimeEntry.find(:all,
-                              :conditions => ["#{TimeEntry.table_name}.user_id = ? AND #{TimeEntry.table_name}.spent_on BETWEEN ? AND ?", @user.id, @filters.from, @filters.to],
+                              :conditions => conditions,
                               :include => [:activity, :project, {:issue => [:tracker, :status]}],
                               :order => "#{TimeEntry.table_name}.spent_on DESC, #{Project.table_name}.name ASC, #{Tracker.table_name}.position ASC, #{Issue.table_name}.id ASC")
 
